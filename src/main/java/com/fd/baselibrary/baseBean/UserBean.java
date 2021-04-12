bc0191c9ee1b51b70c9abfeacb25474348b570a8
孙涛 <suntao@voiceaitech.com>
2021-04-12 17:22:39 +0800

    first commit

---
+++ src/main/java/com/fd/baselibrary/baseBean/UserBean.java
@@ -0,0 +1,271 @@
+package com.fd.baselibrary.baseBean;
+
+import java.io.Serializable;
+
+public class UserBean implements Serializable {
+
+
+    /**
+     * address :
+     * area :
+     * birthDate :
+     * city :
+     * collect : 0
+     * contract : 0
+     * expert : 0
+     * id : 0
+     * image :
+     * info : 0
+     * jobStatus : 0
+     * mail :
+     * name :
+     * phone :
+     * points : 0
+     * province :
+     * resumeId : 0
+     * sex : 0
+     * status : 0
+     * synopsis :
+     * token :
+     * train : 0
+     * type : 0
+     * userSig :
+     * waitingList : 0
+     * workDate :
+     */
+
+    private String address;
+    private String area;
+    private String birthDate;
+    private String city;
+    private Integer collect;
+    private Integer contract;
+    private Integer expert;
+    private Integer id;
+    private String image;
+    private Integer info;
+    private Integer jobStatus;
+    private String mail;
+    private String name;
+    private String phone;
+    private Integer points;
+    private String province;
+    private Integer resumeId;
+    private Integer sex;
+    private Integer status;
+    private String synopsis;
+    private String token;
+    private Integer train;
+    private Integer type;
+    private String userSig;
+    private Integer waitingList;
+    private String workDate;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getCollect() {
+        return collect;
+    }
+
+    public void setCollect(Integer collect) {
+        this.collect = collect;
+    }
+
+    public Integer getContract() {
+        return contract;
+    }
+
+    public void setContract(Integer contract) {
+        this.contract = contract;
+    }
+
+    public Integer getExpert() {
+        return expert;
+    }
+
+    public void setExpert(Integer expert) {
+        this.expert = expert;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer getInfo() {
+        return info;
+    }
+
+    public void setInfo(Integer info) {
+        this.info = info;
+    }
+
+    public Integer getJobStatus() {
+        return jobStatus;
+    }
+
+    public void setJobStatus(Integer jobStatus) {
+        this.jobStatus = jobStatus;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public Integer getResumeId() {
+        return resumeId;
+    }
+
+    public void setResumeId(Integer resumeId) {
+        this.resumeId = resumeId;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Integer getTrain() {
+        return train;
+    }
+
+    public void setTrain(Integer train) {
+        this.train = train;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getUserSig() {
+        return userSig;
+    }
+
+    public void setUserSig(String userSig) {
+        this.userSig = userSig;
+    }
+
+    public Integer getWaitingList() {
+        return waitingList;
+    }
+
+    public void setWaitingList(Integer waitingList) {
+        this.waitingList = waitingList;
+    }
+
+    public String getWorkDate() {
+        return workDate;
+    }
+
+    public void setWorkDate(String workDate) {
+        this.workDate = workDate;
+    }
+}
